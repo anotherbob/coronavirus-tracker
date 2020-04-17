f554f40fa8b9e94340b18eebb5760589513dcff6
Rob <anotherbob@github.com>
2020-04-17 11:05:21 -0600

    Updates

---
+++ precache-manifest.3465fd9510d488dd1b5431cdf48a7c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2198e2e610b34bd0ee52577ebfef1ba",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "b93cc433118b305d8fba",
+    "url": "/coronavirus-tracker/static/css/2.30e53678.chunk.css"
+  },
+  {
+    "revision": "c13046153e5a0cfcc75a",
+    "url": "/coronavirus-tracker/static/css/main.cccbf741.chunk.css"
+  },
+  {
+    "revision": "b93cc433118b305d8fba",
+    "url": "/coronavirus-tracker/static/js/2.06d3c98a.chunk.js"
+  },
+  {
+    "revision": "de6e750a5a912c188b5277edf7888f94",
+    "url": "/coronavirus-tracker/static/js/2.06d3c98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13046153e5a0cfcc75a",
+    "url": "/coronavirus-tracker/static/js/main.6630f760.chunk.js"
+  },
+  {
+    "revision": "cf24b9d25f47a1a04098",
+    "url": "/coronavirus-tracker/static/js/runtime-main.0ea4bdb2.js"
+  }
+]);
